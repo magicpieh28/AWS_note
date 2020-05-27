a8a99f493dc2b5a24ce6e9819e8a67bb549a8c92
magicpieh28 <choi.jungwon.ca1@gmail.com>
2020-05-28 01:20:53 +0900

    Redshift connector using parameter fetcher

---
+++ AWS_note/src/for_Redshift/__init__.py
@@ -0,0 +1,2 @@
+from .connect import RedshiftConnector
+from .unload_and_copy import UnloadAndCopyFromRedshift
---
+++ AWS_note/src/for_Redshift/connect.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+from src import parameter_fetcher
+
+
+class RedshiftConnector:
+    def __init__(self, env: str = 'default'):
+        self.pf = parameter_fetcher.ParameterFetcher(env)
+
+    def do_query(self, query):
+        fetch_all = []
+        cursor = self.get_cursor()
+        cursor.execute(query)
+        fetch_all.append(cursor.fetchall())
+        cursor.close()
+        return fetch_all
+
+    def get_connect_info(self):
+        return {
+            "host": self.parameter_fetcher.fetch_parameters('/datalake/redshift/host'),
+            "port": self.parameter_fetcher.fetch_parameters('/datalake/redshift/port'),
+            "user": self.parameter_fetcher.fetch_parameters('/datalake/redshift/username'),
+            "database": self.parameter_fetcher.fetch_parameters('/datalake/redshift/database'),
+            "password": self.parameter_fetcher.fetch_parameters('/datalake/redshift/password')
+        }
+
+    def connect(self):
+        return psycopg2.connect(**self.get_connect_info())
+
+    def get_cursor(self):
+        return self.connect().cursor()
